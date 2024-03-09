a3c2fb4ea2b26df7d94c5f6304cc15d78c65cb02
gaurisankartarasia <157940097+gaurisankartarasia@users.noreply.github.com>
2024-03-09 10:31:48 +0530

    Add files via upload

---
+++ badamba.js
@@ -0,0 +1,16 @@
+// hamburger menu
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
+// scroll to particular sectioin--------------
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
