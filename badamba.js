0c5af917f0684cb4f1f2ab913eaad83201a91ec7
gaurisankartarasia <157940097+gaurisankartarasia@users.noreply.github.com>
2024-03-15 12:26:16 +0530

    Update badamba.js

--- badamba.js
+++ badamba.js
@@ -1,16 +1,34 @@
 // hamburger menu
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleButton = document.querySelector('.dropdown-toggle');
+  const dropdownMenu = document.querySelector('.dropdown-menu');
+  let menuOpened = false;
 
-// scroll to particular sectioin--------------
-function scrollToSection(sectionId) {
-  const section = document.getElementById(sectionId);
-  if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-  }
-}
+  toggleButton.addEventListener('click', function() {
+    if (!menuOpened) {
+      dropdownMenu.style.display = 'block';
+      anime({
+        targets: '.dropdown-menu',
+        opacity: 1,
+        translateY: '0px',
+        duration: 300,
+        easing: 'easeOutQuad'
+      });
+      menuOpened = true;
+    } else {
+      anime({
+        targets: '.dropdown-menu',
+        opacity: 0,
+        translateY: '-10px',
+        duration: 300,
+        easing: 'easeOutQuad',
+        complete: function() {
+          dropdownMenu.style.display = 'none';
+        }
+      });
+      menuOpened = false;
+    }
+  });
+});
+
